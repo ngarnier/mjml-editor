9888678de75b9f05a28cd54b11088bde2c477cc2
Loëck Vézien <nainpuissant@gmail.com>
2017-01-04 23:36:11 +0100

    First route gist

--- src/server/github.js
+++ src/server/github.js
@@ -5,14 +5,20 @@ const router = express.Router()
 
 const github = new GitHubApi()
 
-router.get('/test', (req, res) => {
+router.get('/gists/:id', (req, res) => {
+  const {
+    id,
+  } = req.params
+
   github.authenticate({
     type: 'token',
     token: req.user.accessToken,
   })
 
-  github.gists.getAll({}, (err, data) => {
-    console.log(err, data)
+  github.gists.get({
+    id,
+  }, (err, data) => {
+    res.send(data)
   })
 })
 

7252e1be4fcf5fe837518b2696beb93db8a6507a
meriadec <meriadec.pillet@gmail.com>
2017-01-18 22:08:12 +0100

    redirect to gist when saving a new gist

--- src/actions/gists.js
+++ src/actions/gists.js
@@ -40,25 +40,44 @@ export function loadGist (gistID) {
 // if gist already exists for this session, use it
 // else, create a new gist for it
 export function saveCurrentTabToGist () {
-  return {
-    type: 'API:SAVE_CURRENT_TO_GIST',
-    payload: {
-      method: 'post',
-      url: '/github/gists',
-      data: state => {
+  return function (dispatch, getState) {
 
-        const gistID = state.gist.get('id')
-        const tabs = state.editor.get('tabs')
-        const activeTab = state.editor.get('activeTab')
-        const tab = tabs.get(tabs.findIndex(t => t.get('id') === activeTab))
+    const action = {
+      type: 'API:SAVE_CURRENT_TO_GIST',
+      payload: {
+        method: 'post',
+        url: '/github/gists',
+        data: state => {
 
-        return {
-          gistID,
-          tab,
-        }
+          const gistID = state.gist.get('id')
+          const tabs = state.editor.get('tabs')
+          const activeTab = state.editor.get('activeTab')
+          const tab = tabs.get(tabs.findIndex(t => t.get('id') === activeTab))
 
+          return {
+            gistID,
+            tab,
+          }
+
+        },
       },
-    },
+    }
+
+    return dispatch(action)
+
+      // if there is no current gist, redirect to newly created gist
+      .then(res => {
+        const hasGist = !!getState().gist.get('id')
+        if (!hasGist) {
+          const { gistID } = res
+
+          // faster than waiting for socket :D
+          history.replaceState({}, '', `/gists/${gistID}`)
+
+          return dispatch(loadGist(gistID))
+        }
+      })
+
   }
 }
 
